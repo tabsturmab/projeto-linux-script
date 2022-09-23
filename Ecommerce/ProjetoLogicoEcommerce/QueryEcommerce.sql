cea4dca065008ae1be17d96f0027f40db8800c67
unknown <Andre Silva>
2022-09-23 16:41:30 -0300

    Correção Query

--- Ecommerce/ProjetoLogicoEcommerce/QueryEcommerce.sql
+++ Ecommerce/ProjetoLogicoEcommerce/QueryEcommerce.sql
@@ -1,19 +1,19 @@
-/* Recuperações simples com SELECT Statement  - Listar todos Clientes*/
-SELECT * FROM `Cliente`;
-
-/*Filtros com WHERE Statement - Listar todos Clientes com filtro de casa*/
+/*Listar Clientes com filtro de residencia*/
 SELECT * FROM `Cliente` WHERE `IdClienteTipo`='1';
 
-/*Crie expressões para gerar atributos derivados - Listar todos Clientes com endereço*/
+/*Expressões para gerar atributos e Listar todos Clientes com endereço*/
 SELECT * FROM Cliente, `ClienteEndereco` WHERE `IdClienteEndereco`=IdCliente;
 
-/*Defina ordenações dos dados com ORDER BY - Listar todos Clientes com endereço e lista por order alfabetica*/
+/* Recuperações simples Clientes*/
+SELECT * FROM `Cliente`;
+
+/*Ordenações dos dados com ORDER BY e Listar todos Clientes com endereço por ordem alfabetica*/
 SELECT * FROM Cliente, `ClienteEndereco` WHERE `IdClienteEndereco`=IdCliente ORDER BY Nome DESC;
 
-/*Filtros aos grupos – HAVING Statement - Listar o Total de clientes por tipo PF/PJ e listar o que tiver mais que 1*/
+/*Listar o Total de clientes por tipo PF/PJ e listar o que tiver mais que 1*/
 SELECT COUNT(*) AS total, `IdCliente Tipo` FROM `Cliente` GROUP BY `IdCliente Tipo` HAVING total > 1;
 
-/*Junções entre tabelas para fornecer uma perspectiva mais complexa dos dados - Listar Nome, Tipo PF/PJ, CEP, Nº, Tipo Endereço*/
+/*Junções entre tabelas e Listar Nome, Tipo PF/PJ, CEP, Nº, Tipo Endereço*/
 SELECT a.Nome,c.Tipo,b.CEP,b.Numero,d.Tipo AS "Tipo Endereco" FROM Cliente AS a INNER JOIN `ClienteEndereco` AS b ON b.`IdClienteEndereco`=a.IdCliente 
 INNER JOIN `ClienteTipo` AS c ON c.`IdClienteTipo`=a.`IdClienteTipo` LEFT JOIN `TipoEndereco` AS d ON d.`IdTipoEndereco`=b.`IdClienteEndereco`
 ORDER BY a.Nome DESC;
